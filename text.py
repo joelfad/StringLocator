bb6379393aad714397bae327c702fdc79d06ead3
Joel McFadden <mcf.joel@gmail.com>
2018-09-27 17:09:54 -0600

    refactor text logic into basic classes

--- text.py
+++ text.py
@@ -10,29 +10,43 @@ from nltk.tokenize import PunktSentenceTokenizer
 import re
 
 
-# loads text from a file
-# TODO: is there a way to search the text without loading it? (scalability)
-def load(filename):
-    with open(filename) as f:
-        return f.read()
+TEXT_PATH = './resources/text/king-easy-test.txt'
 
 
-# small tests
-text = load('resources/text/king-easy-test.txt')
+class SearchableText:
+    def __init__(self, filename):
+        # TODO: is there a way to search the text without loading it? (scalability)
 
-sentences = sent_tokenize(text)
-counter = 0
-for s in sentences:
-    print('[{}]'.format(counter))
-    print(s)
-    counter = counter + 1
+        with open(filename) as f:
+            self.text = f.read()
+        self.boundaries = PunktSentenceTokenizer().span_tokenize(self.text)
 
-pst = PunktSentenceTokenizer()
+        # debug 1
+        sentences = sent_tokenize(self.text)
+        counter = 0
+        for s in sentences:
+            print('[{}]'.format(counter))
+            print(s)
+            counter = counter + 1
 
-boundaries = pst.span_tokenize(text)
-for b in boundaries:
-    print(b)
+    def text(self):
+        return self.text
+
+    def last_boundary(self):
+        # debug 2
+        for b in self.boundaries:
+            print(b)
+        return b
+
+
+class QueryResult:
+    def __init__(self, query):
+        self.query = query
+
+
+s = SearchableText(TEXT_PATH)
+q = QueryResult("beacon")
 
 
 # grab sentence from text and replace CRLF with spaces
-print(re.sub(r"\r?\n?", "", text[slice(*b)]))
+print(re.sub(r"\r?\n", " ", s.text[slice(*s.last_boundary())]))
