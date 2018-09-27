fdb1f3890ee5d103e783f8431b3d366f26f432e0
Joel McFadden <mcf.joel@gmail.com>
2018-09-27 14:59:51 -0600

    return sample JSON, add text file

--- app.py
+++ app.py
@@ -1,11 +1,41 @@
-from flask import Flask
+from flask import Flask, url_for, jsonify
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return 'index'
+
+
+@app.route('/search')
+def search():
+    return 'search'
+
+
+# sample return JSON
+@app.route('/search/<query>')
+def find(query):
+    return jsonify(
+        query_text=query,
+        number_of_occurrences=3,
+        occurrences=[
+            {
+                "line": 1,
+                "start": 2,
+                "end": 3,
+                "in_sentence": "Test sentence."
+            }
+        ]
+    )
+
+
+# sample showing URLs
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('search'))
+    print(url_for('search', next='/'))
+    print(url_for('find', query='Now'))
 
 
 if __name__ == '__main__':
