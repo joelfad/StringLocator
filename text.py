7ee6a5ed675802d08f3d1698ab47d5fbd44bf093
Joel McFadden <mcf.joel@gmail.com>
2018-09-27 22:56:13 -0600

    tidy up code a bit

--- text.py
+++ text.py
@@ -5,7 +5,6 @@
 # Text file loading and search algorithms
 #
 
-from nltk.tokenize import sent_tokenize
 from nltk.tokenize import PunktSentenceTokenizer
 import re
 
@@ -15,37 +14,25 @@ TEXT_PATH = './resources/text/king-easy-test.txt'
 
 class SearchableText:
     def __init__(self, filename):
-        # TODO: is there a way to search the text without loading it? (scalability)
-
+        # load file
         with open(filename) as f:
             self.text = f.read()
-        self.boundaries = PunktSentenceTokenizer().span_tokenize(self.text)
 
-        # debug 1
-        # sentences = sent_tokenize(self.text)
-        # counter = 0
-        # for s in sentences:
-        #     print('[{}]'.format(counter))
-        #     print(s)
-        #     counter = counter + 1
+        # detect sentence boundaries
+        self.boundaries = PunktSentenceTokenizer().span_tokenize(self.text)
 
     def text(self):
         return self.text
 
-    def last_boundary(self):
-        # debug 2
-        for b in self.boundaries:
-            print(b)
-        return b
-
     def process_query(self, query_text):
+        """
+        TODO: Add documentation
+
+        :param query_text:
+        :return:
+        """
 
         occurrences = self.find_occurrences(query_text)
-
-        # debug
-        # for o in occurrences:
-        #     print(o)
-
         return {
             "query_text": query_text,
             "number_of_occurrences": len(occurrences),
@@ -53,29 +40,35 @@ class SearchableText:
         }
 
     def find_occurrences(self, query_text):
+        """
+        TODO: Add documentation
+
+        :param query_text:
+        :return:
+        """
 
         occurrences = []
-
-        pattern = re.compile(query_text)
 
         # keep running count of characters from previous lines (including newlines)
         # for pairing matches with sentences
         chars_on_prev_lines = 0
 
         lines_of_text = self.text.split('\n')
-        print(lines_of_text)
 
+        pattern = re.compile(query_text)
         for line_number, line in enumerate(lines_of_text, 1):
 
+            # debug
             print("Searching line {}...".format(line_number))
             print("({} chars from previous lines)\n".format(chars_on_prev_lines))
-
 
             # matches are returned in order found, left to right
             matches = re.finditer(pattern, line)
             for m in matches:
+
+                # debug
                 print(m)
-                # print("line_number: {}".format(line_counter + 1))
+
                 char_index = chars_on_prev_lines + m.start()
                 occurrences.append(
                     {
@@ -95,12 +88,18 @@ class SearchableText:
         """
         Generator to pair matches with sentences
         """
-        for b in self.boundaries:
-            print(b)
-            while char_index in range(*b):
-                print("{} is in the range({}, {})\n".format(char_index, *b))
+        for boundary in self.boundaries:
+
+            # debug
+            print(boundary)
+
+            while char_index in range(*boundary):
+
+                # debug
+                print("{} is in the range{}\n".format(char_index, boundary))
+
                 # grab sentence from text and replace CRLF with spaces
-                yield re.sub(r"\r?\n", " ", self.text[slice(*b)])
+                yield re.sub(r"\r?\n", " ", self.text[slice(*boundary)])
 
 
 # debug - DEMO
@@ -108,4 +107,3 @@ s = SearchableText(TEXT_PATH)
 r = s.process_query("This")
 for o in r["occurrences"]:
     print(o)
-
